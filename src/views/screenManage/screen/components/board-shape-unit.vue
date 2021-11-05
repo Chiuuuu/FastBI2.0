63cd545021b8ef7f2101bb77d940bd286d8710c4
zhaowk <ckt5532@dingtalk.com>
2021-11-05 15:49:06 +0800

    fix: 补充遗漏的缩放比修改

--- src/views/screenManage/screen/components/board-shape-unit.vue
+++ src/views/screenManage/screen/components/board-shape-unit.vue
@@ -164,7 +164,7 @@ export default {
   },
   computed: {
     ...mapState({
-      boardScale: state => state.board.scale,
+      boardScale: state => state.board.page.size.scale,
       model: state => state.board.model,
       componentsOrigin: state => state.board.components,
     }),
